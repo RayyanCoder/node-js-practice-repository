0fbe95ee76935aa4e01d8c14be72e6afd0a5607b
rayyanlearning <rayyanlearning2022@gmail.com>
2022-10-19 07:33:52 -0700

    api feature completed
    Api feature completed with file

--- Controllers/toursController.js
+++ Controllers/toursController.js
@@ -2,19 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const tourModel = require('./../models/tourModel');
 const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+const ApiFeatures = require('./../utils/apiFeatures');
 
 
-exports.createTour = async(req,res)=>{
+exports.createTour = catchAsync(async(req,res)=>{
     console.log('tours is created');
-        const createdTour = await tourModel.create(req.body);
+    const createdTour = await tourModel.create(req.body);
     res.status(200).json({
         body:"hello world api",
         data:createdTour
     });
 
-}
+});
 
-exports.updateTour = async(req,res,next)=>{
+exports.updateTour = catchAsync(async(req,res,next)=>{
 const  tour = await tourModel.findByIdAndUpdate(req.params.id,req.body,{
     new:true,
     runValidators:true
@@ -27,26 +29,30 @@ res.status(200).json({
     tours:tour
 });
 
-}
+});
 
-exports.getTour = async(req,res,next)=>{
+exports.getTour = catchAsync(async(req,res,next)=>{
     const tour = await tourModel.findById(req.params.id);
+    if(!tour){
+        next(new AppError('record not found',404));
+    }
     res.status(200).json({
         tours:tour
     });
-}
+});
 
-exports.getAllTours = async(req,res,next)=>{
+exports.getAllTours = catchAsync(async(req,res,next)=>{
+    // const  features = new ApiFeatures(tourModel.find(),req.query).filter();
     const tours = await tourModel.find();
     res.status(200).json({
         allTours:tours
     });
-}
+});
 
-exports.deleteTour = async(req,res,next)=>{
+exports.deleteTour = catchAsync(async(req,res,next)=>{
     const tour = await tourModel.findByIdAndDelete(req.params.id);
     res.status(200).json({
         message:'deleted successfully',
         tour:null
     });
-}
+});
